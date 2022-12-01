fe4f3a62f20c6000e9de27dcab3f3d159e8e3c12
Vladimir Rudnev <vladimir.rudnev@jetbrains.com>
2022-12-01 12:06:33 +0200

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,10 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+
+    fun solve(input: List<List<Int>>, count: Int): Int {
+        return input.map { it.sum() }.sortedDescending().take(count).sum()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    val input = readNumbersGroups("Day01")
+    println(solve(input, 1))
+    println(solve(input, 3))
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -2,11 +2,28 @@ import java.io.File
 import java.math.BigInteger
 import java.security.MessageDigest
 
+const val NewLine = "\r\n"
+
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = File("src", "$name.txt")
+fun readInput(name: String) = File("inputs", "$name.txt")
     .readLines()
+
+/**
+ * Reads numbers from the given input txt file.
+ */
+fun readNumbers(name: String) = File("inputs", "$name.txt")
+        .readLines()
+        .map { it.toIntOrNull() }
+
+/**
+ * Reads numbers groups from the given input txt file.
+ */
+fun readNumbersGroups(name: String, separator: String = "$NewLine$NewLine") = File("inputs", "$name.txt")
+    .readText()
+    .split(separator)
+    .map { it.lines().map { it.toInt() } }
 
 /**
  * Converts string to md5 hash.
